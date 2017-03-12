ce5a7103c97acf034d73965bb05ed8293efd13c2
Kevin Glass <Kevin Glass>
2017-03-12 18:00:26 -0400

    test for win in 2nd position with x

--- test_unbeatable.rb
+++ test_unbeatable.rb
@@ -8,4 +8,12 @@ class TestUnbeatable < Minitest::Test
 		assert_equal('X', player.marker)
 	end
 
+	def test_win_at_second_pos_with_x
+		player = Unbeatable.new('X')
+		board_array = ['X','X','','','','','','','']
+		assert_equal(2, player.get_move(board_array))
+	end
+
+	
+
 end
