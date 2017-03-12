ae734792e509a7ca97b31e31bf1c770e60b7171c
Kevin Glass <Kevin Glass>
2017-03-12 17:58:57 -0400

    test for create player

--- test_unbeatable.rb
+++ test_unbeatable.rb
@@ -3,72 +3,9 @@ require_relative 'unbeatable.rb'
 
 class TestUnbeatable < Minitest::Test
 
-
-
 	def test_create_player
 		player = Unbeatable.new('X')
 		assert_equal('X', player.marker)
-	end
-
-
-	def test_win_at_2nd_pos_with_x
-		player = Unbeatable.new('X')
-		board_array = ['X','X','','','','','','','']
-		assert_equal(2, player.get_move(board_array))
-	end
-
-	def test_win_at_last_position_with_x
-		player = Unbeatable.new('X')
-		board_array = ['','','','','','','','X','X']
-		assert_equal(6, player.get_move(board_array))
-	end
-
-	def test_win_at_4
-		player = Unbeatable.new('X')
-		board_array = ['X','','','','','','','','X']
-		assert_equal(4, player.get_move(board_array))
-	end
-
-	def test_no_win
-		player = Unbeatable.new('X')
-		board_array = ['x','','','','','','','','']
-		assert_equal(1, player.get_move(board_array))
-	end
-
-	def test_win_at_5
-		player = Unbeatable.new('O')
-		board_array = ['','','','O','O','','','','']
-		assert_equal(5, player.get_move(board_array))
-	end
-
-	def test_win_at_last_with_o
-		player = Unbeatable.new('O')
-		board_array = ['','','','','','','','O','O']
-		assert_equal(6, player.get_move(board_array))
-	end
-
-	def test_block1
-		player = Unbeatable.new('O')
-		board_array = ['X','X','','','','','','','']
-		assert_equal(2, player.get_move(board_array))
-	end
-
-	def test_block2
-		player = Unbeatable.new('X')
-		board_array = ['O','','','','O','','','X','']
-		assert_equal(8, player.get_move(board_array))
-	end
-
-	def test_block3
-		player = Unbeatable.new('X')
-		board_array = ['','','O','','','O','','X','']
-		assert_equal(8, player.get_move(board_array))
-	end
-
-	def test_block4
-		player = Unbeatable.new('O')
-		board_array = ['X','','X','','X','O','','X','O']
-		assert_equal(6, player.get_move(board_array))
 	end
 
 end
--- unbeatable.rb
+++ unbeatable.rb
@@ -1,18 +1,13 @@
 class Unbeatable
 		attr_accessor :marker
 
-
-
 	def initialize(marker)
 		@marker = marker
-
 	end
 
 	def get_move(ttt_board)
-
 		win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 		#win_array.each do |win|
-
 		 board_position = [
 			[ttt_board[0], ttt_board[1], ttt_board[2]],
 			[ttt_board[3], ttt_board[4], ttt_board[5]],
@@ -22,38 +17,81 @@ class Unbeatable
 			[ttt_board[2], ttt_board[5], ttt_board[8]],
 			[ttt_board[0], ttt_board[4], ttt_board[8]],
 			[ttt_board[2], ttt_board[4], ttt_board[6]]
-
 					]
+
 				marker_1 = 'X'
 				marker_2 = 'O'
 
-			if marker == marker_1
-			   opponent = marker_2
-			else
-				opponent = marker_1
-			end
+				if marker == marker_1
+					opponent = marker_2
+				else
+					opponent = marker_1
+				end
 
-				results = ttt_board.index('')
-					
-				board_position.each_with_index do |win_combo, index|
-
-					if 
+			results = ttt_board.index('')
+			board_position.each_with_index do |win_combo, index|
+			
+				if 
 					win_combo.count(marker) == 2 &&win_combo.count('') == 1
-						winning_pos = win_combo.index('') 
-							results = win_array[index][winning_pos]
+					winning_pos = win_combo.index('') 
+					results = win_array[index][winning_pos]
 
-					elsif win_combo.count(opponent) == 2 && win_combo.count('') == 1
-					 		winning_pos = win_combo.index('')
-					 		   results = win_array[index][winning_pos] 
-
-					else 
-
-						results
-
-					end
-
-	end
+				elsif win_combo.count(opponent) == 2 && win_combo.count('') == 1
+					winning_pos = win_combo.index('')
+					results = win_array[index][winning_pos] 
+				else 
+					results
+				end
+			end
 		results
 	end
 
+	def check_for_fork(ttt_board)
+
+		fork_combinations = [
+							[ttt_board[0],ttt_board[1],ttt_board[2]],
+							[ttt_board[3],ttt_board[4],ttt_board[5]],
+							[ttt_board[6], ttt_board[7], ttt_board[8]],
+							[ttt_board[0], ttt_board[3], ttt_board[6]],
+							[ttt_board[1],ttt_board[4], ttt_board[7]],
+							[ttt_board[2],ttt_board[5],ttt_board[8]], 
+							[ttt_board[0], ttt_board[4], ttt_board[8]],
+							[ttt_board[2],ttt_board[4],ttt_board[6]]
+							]
+
+		fork_positions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+		fork_line = []
+		fork_spot = [] # new array for when you flatten
+		i = []  #index positions
+
+		fork_combinations.each_with_index do |forking_line, index|
+			if forking_line.count(marker) == 1 && forking_line.count("") == 2
+				fork_line = forking_line
+				i.push(index)
+			end
+		end 
+
+		i.each do |index|
+			fork_spot << fork_positions[index]
+		end
+		p fork_spot
+		fork_spot = fork_spot.flatten.sort #flattens array, takes forks position arrays, puts in fork positions array
+
+		intersections = []
+		fork_spot.each do |spot| 
+			if ttt_board[spot] == ""
+				intersections.push(spot)
+			end
+		end
+
+		if intersections.detect { |match| intersections.count(match) > 1 } == nil
+
+			move = 10
+
+		else
+			move = intersections.detect { |match| intersections.count(match) > 1}
+		end
+		move
+	end
 end
