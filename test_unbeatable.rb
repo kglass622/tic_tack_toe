48760938839f233ba58ee7b2a422fd34bb0547f9
Kevin Glass <Kevin Glass>
2017-03-13 14:19:05 -0400

    changed code to match in class work

--- test_unbeatable.rb
+++ test_unbeatable.rb
@@ -1,146 +1,431 @@
 require 'minitest/autorun'
+
 require_relative 'unbeatable.rb'
+
+
 
 class TestUnbeatable < Minitest::Test
 
-	def test_create_player
+
+
+	def test_new_player_X
+
 		player = Unbeatable.new('X')
+
 		assert_equal('X', player.marker)
+
 	end
 
-	def test_win_at_second_pos_with_x
+
+
+	def test_for_win_at_2
+
 		player = Unbeatable.new('X')
-		board_array = ['X','X','','','','','','','']
-		assert_equal(2, player.get_move(board_array))
+
+		ttt_board = ['X', 'X', '', '', '', '', '', '', '']
+
+		assert_equal(2, player.get_win(ttt_board))
+
 	end
 
-	def test_win_at_last_position_with_x
+
+
+	def test_win_at_last_pos_with_x
+
 		player = Unbeatable.new('X')
-		board_array = ['','','','','','','','X','X']
-		assert_equal(6, player.get_move(board_array))
+
+		ttt_board = ['', '', '', '', '', '', '', 'X', 'X']
+
+		assert_equal(6, player.get_win(ttt_board))
+
 	end
+
+
 
 	def test_win_at_4
-		player = Unbeatable.new('X')
-		board_array = ['X','','','','','','','','X']
-		assert_equal(4, player.get_move(board_array))
+
+		player = Unbeatable.new('O')
+
+		ttt_board = ['', '', '', 'O', '', 'O', '', '', '']
+
+		assert_equal(4, player.get_win(ttt_board))
+
 	end
+
+
 
 	def test_no_win
+
 		player = Unbeatable.new('X')
-		board_array = ['x','','','','','','','','']
-		assert_equal(1, player.get_move(board_array))
+
+		ttt_board = ['X', '', '', '', '', '', '', '', '']
+
+		assert_equal(9, player.get_win(ttt_board))
+
 	end
 
-	def test_win_at_5
+
+
+	def test_O_wins_at_8
+
 		player = Unbeatable.new('O')
-		board_array = ['','','','O','O','','','','']
-		assert_equal(5, player.get_move(board_array))
-	end
-	
-	def test_win_at_last_with_o
-		player = Unbeatable.new('O')
-		board_array = ['','','','','','','','O','O']
-		assert_equal(6, player.get_move(board_array))
+
+		ttt_board = ['', '', '', '', '', '', 'O', 'O', '']
+
+		assert_equal(8, player.get_win(ttt_board))
+
 	end
 
-	def test_block1
-		player = Unbeatable.new('O')
-		board_array = ['X','X','','','','','','','']
-		assert_equal(2, player.get_move(board_array))
-	end
 
-	def test_block2
+
+	def test_block_at_8
+
 		player = Unbeatable.new('X')
-		board_array = ['O','','','','O','','','X','']
-		assert_equal(8, player.get_move(board_array))
+
+		ttt_board = ['O', 'O', '', '', '', '', '', '', '']
+
+		assert_equal(2, player.get_block(ttt_board))
+
 	end
 
-	def test_block3
+
+
+	def test_block_2
+
 		player = Unbeatable.new('X')
-		board_array = ['','','O','','','O','','X','']
-		assert_equal(8, player.get_move(board_array))
+
+		ttt_board = ['O', '', '', '', 'O', '', '', 'X', '']
+
+		assert_equal(8, player.get_block(ttt_board))
+
 	end
 
-	def test_block4
-		player = Unbeatable.new('O')
-		board_array = ['X','','X','','X','O','','X','O']
-		assert_equal(6, player.get_move(board_array))
-	end
 
-	def test_fork_1
+
+	def test_block_3
+
 		player = Unbeatable.new('X')
-		board_array = ['X','O','','O','','','X','','']
-		assert_equal(4, player.check_for_fork(board_array))
+
+		ttt_board = ['O', '', '', '', 'O', '', '', 'X', '']
+
+		assert_equal(8, player.get_block(ttt_board))
+
 	end
 
-	def test_fork_2
-		player = Unbeatable.new('O')
-		board_array = ['','x','','','O','X','','O','']
-		assert_equal(6, player.check_for_fork(board_array))
-	end
 
-def test_block_fork
-		player = Unbeatable.new('O')
-		board_array = ['', 'X', '', '', 'O', 'X', '', 'O', '']
-		assert_equal(2, player.block_fork(board_array))
-	end
 
-	def test_block_fork_2
+	def test_fork_win
+
 		player = Unbeatable.new('X')
-		board_array = ['O', 'X', '', 'X', '', '', 'O', '', '']
-		assert_equal(4, player.block_fork(board_array))
+
+		ttt_board = ['X', 'O', '', 'O', '', '', 'X', '', '']
+
+		assert_equal(4, player.win_check_fork(ttt_board))
+
 	end
 
-	def test_for_block_3
+
+
+	def test_fork_win_2
+
 		player = Unbeatable.new('X')
-		board_array = ['X', 'O', '', 'O', '', '', '', '', 'X']
-		assert_equal(4, player.block_fork(board_array))
+
+		ttt_board = ['X', '', '', '', 'O', '', '', '', 'X']
+
+		assert_equal(2, player.win_check_fork(ttt_board))
+
 	end
 
-	def test_block_fork_4
+
+
+	def test_fork_block_1
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['', 'O', '', '', 'X', 'O', '', 'X', '']
+
+		assert_equal(2, player.block_check_fork(ttt_board))
+
+	end
+
+
+
+	def test_fork_block_2
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', 'O', '', '', '', '', 'X']
+
+		assert_equal(4, player.block_check_fork(ttt_board))
+
+	end
+
+
+
+	def test_fork_block_3
+
 		player = Unbeatable.new('O')
-		board_array = ['', 'O', '', 'X', 'O', '', '', 'X', '']
-		assert_equal(6, player.block_fork(board_array))
+
+		ttt_board = ['X', 'O', '', 'O', '', '', 'X', '', '']
+
+		assert_equal(4, player.block_check_fork(ttt_board))
+
 	end
 
-	#TAKE CENTER
+
+
+	def test_weird_situation
+
+		player = Unbeatable.new('O')
+
+		ttt_board = ['X', '', '', '', 'O', '', '', '', 'X']
+
+		assert_equal(3, player.block_check_fork(ttt_board))
+
+	end
+
+
 
 	def test_take_center
-		player = Unbeatable.new('O')
-		board_array = ['', '', '', '', '', '', '', '', '']
-		assert_equal(4, player.take_center(board_array))
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', 'O', '', '', 'X', '', '']
+
+		assert_equal(4, player.take_center(ttt_board))
+
 	end
+
+
 
 	def test_take_center_2
+
 		player = Unbeatable.new('X')
-		board_array = ['', '', 'X', 'O', '', '', 'X', 'O', '']
-		assert_equal(4, player.take_center(board_array))
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', '']
+
+		assert_equal(9, player.take_center(ttt_board))
+
 	end
 
-	def test_take_filled_center_return_10
-		player = Unbeatable.new('X')
-		board_array = ['', '', 'X', 'O', 'O', '', 'X', 'O', '']
-		assert_equal(10, player.take_center(board_array))
-	end
 
-	#TAKE CORNER
 
-	def test_take_corner
-		player = Unbeatable.new('X')
-		board_array = ['O', '', '', '', '', '', '', '', '']
-		assert_equal(8, player.opposite_corner(board_array))
-	end
+	def test_take_center_3
 
-	def test_take_corner_2
 		player = Unbeatable.new('O')
-		board_array = ['', '', '', '', '', '', '', '', 'X']
-		assert_equal(0, player.opposite_corner(board_array))
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', '']
+
+		assert_equal(9, player.take_center(ttt_board))
+
 	end
 
-	
+
+
+	def test_take_opposite_corner
+
+		player = Unbeatable.new('O')
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', '']
+
+		assert_equal(8, player.take_corner(ttt_board))
+
+	end
+
+
+
+	def test_take_opposite_corner_O
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['O', 'X', '', 'X', 'O', '', 'O', '', '']
+
+		assert_equal(8, player.take_corner(ttt_board))
+
+	end
+
+
+
+	def test_take_opposite_corner_6
+
+		player = Unbeatable.new('O')
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', 'X']
+
+		assert_equal(2, player.take_corner(ttt_board))
+
+	end
+
+
+
+	def test_take_opposite_corner_zero
+
+		player = Unbeatable.new('O')
+
+		ttt_board = ['', 'O', 'O', 'O', 'X', '', 'X', '', 'X']
+
+		assert_equal(0, player.take_corner(ttt_board))
+
+	end
+
+
+
+	def test_take_opposite_corner_six
+
+		player = Unbeatable.new('O')
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', 'X']
+
+		assert_equal(2, player.take_corner(ttt_board))
+
+	end
+
+
+
+	def test_take_opposite_corner_two
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', 'X']
+
+		assert_equal(2, player.take_empty_corner(ttt_board))
+
+	end
+
+
+
+	def test_take_empty_side_two
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', 'O', 'X', '', 'X', '', 'X']
+
+		assert_equal(5, player.take_empty_side(ttt_board))
+
+	end
+
+
+
+	def test_take_empty_side_three
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'X', '', 'O', 'X', 'O', 'X', 'X', 'X']
+
+		assert_equal(10, player.take_empty_side(ttt_board))
+
+	end
+
+
+
+	def test_take_the_win
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'X', '', 'O', 'X', 'O', 'X', '', 'O']
+
+		assert_equal(2, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_block_the_win
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['', 'X', '', '', 'X', 'O', 'O', '', 'O']
+
+		assert_equal(7, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_block_the_win_2
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['O', 'X', '', '', '', '', '', '', 'O']
+
+		assert_equal(4, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_fork_the_win
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', 'O', '', '', 'X', '', '']
+
+		assert_equal(4, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_block_fork_44
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', 'O', '', '', '', '', '']
+
+		assert_equal(4, player.get_move(ttt_board))
+
+	end
+
+
+
+
+
+	def test_take_center_again
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', '', '', '', '', '', '', '']
+
+		assert_equal(4, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_take_corner_44
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', 'O', 'X', 'O', 'X', '', '', '', '']
+
+		assert_equal(6, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_take_empty_side_44
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['X', '', 'O', 'V', 'O', '', 'X', 'e', 'X']
+
+		assert_equal(1, player.get_move(ttt_board))
+
+	end
+
+
+
+	def test_take_empty_random_44
+
+		player = Unbeatable.new('X')
+
+		ttt_board = ['e', 'h', 'e', 'V', 'O', 'h', 'e', '', 'e']
+
+		assert_equal(7, player.get_move(ttt_board))
+
+	end
+
+
+
 end
-	
-	
-	
-	
--- unbeatable.rb
+++ unbeatable.rb
@@ -1,220 +1,738 @@
 class Unbeatable
-		attr_accessor :marker
+
+
+
+	attr_accessor :marker
+
+
 
 	def initialize(marker)
+
 		@marker = marker
+
 	end
+
+
 
 	def get_move(ttt_board)
-		win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-		#win_array.each do |win|
-		 board_position = [
-			[ttt_board[0], ttt_board[1], ttt_board[2]],
-			[ttt_board[3], ttt_board[4], ttt_board[5]],
-			[ttt_board[6], ttt_board[7], ttt_board[8]],
-			[ttt_board[0], ttt_board[3], ttt_board[6]],
-			[ttt_board[1], ttt_board[4], ttt_board[7]],
-			[ttt_board[2], ttt_board[5], ttt_board[8]],
+
+		if get_win(ttt_board) < 9
+
+			p 'win'
+
+			move = get_win(ttt_board)
+
+		elsif
+
+			get_block(ttt_board) < 9
+
+			p 'block'
+
+			move = get_block(ttt_board)
+
+		elsif 
+
+			win_check_fork(ttt_board) < 9
+
+			p 'fork'
+
+			move = win_check_fork(ttt_board)
+
+			
+
+		elsif 
+
+			block_check_fork(ttt_board) < 9
+
+			p 'block fork'
+
+			move = block_check_fork(ttt_board)
+
+			
+
+
+
+		elsif 
+
+			take_center(ttt_board) < 9
+
+			p 'center'
+
+			move = take_center(ttt_board)
+
+			
+
+		elsif 
+
+			take_corner(ttt_board) < 9
+
+			p 'corner'
+
+			move = take_corner(ttt_board)
+
+			
+
+		elsif 
+
+			take_empty_corner(ttt_board) < 9
+
+			p 'empty corner'
+
+			move = take_empty_corner(ttt_board)
+
+			
+
+
+
+		elsif 
+
+			take_empty_side(ttt_board) < 9
+
+			p 'side'
+
+			move = take_empty_side(ttt_board)
+
+			
+
+
+
+		else	
+
+			p 'over'
+
+			move = ttt_board.index('')
+
+			
+
+		end
+
+		
+
+	end
+
+
+
+
+
+	def get_win(ttt_board)
+
+		win_array = [
+
+			[0, 1, 2], 
+
+			[3, 4, 5], 
+
+			[6, 7, 8], 
+
+			[0, 3, 6], 
+
+			[1, 4, 7], 
+
+			[2, 5, 8], 
+
+			[0, 4, 8], 
+
+			[2, 4, 6]
+
+			]
+
+		# win_array.each do |win|
+
+			board_position = [
+
+			[ttt_board[0], ttt_board[1], ttt_board[2]], 
+
+			[ttt_board[3], ttt_board[4], ttt_board[5]], 
+
+			[ttt_board[6], ttt_board[7], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[3], ttt_board[6]], 
+
+			[ttt_board[1], ttt_board[4], ttt_board[7]], 
+
+			[ttt_board[2], ttt_board[5], ttt_board[8]], 
+
 			[ttt_board[0], ttt_board[4], ttt_board[8]],
+
 			[ttt_board[2], ttt_board[4], ttt_board[6]]
+
 					]
 
-				marker_1 = 'X'
-				marker_2 = 'O'
 
-				if marker == marker_1
-					opponent = marker_2
-				else
-					opponent = marker_1
-				end
 
-			results = ttt_board.index('')
-			board_position.each_with_index do |win_combo, index|
+				marker1 = 'X'
+
+				marker2 = 'O'
+
+
+
+					if marker == marker1
+
+						opponent = marker2
+
+					else 
+
+						opponent = marker1
+
+					end
+
+				
+
+			results = 9
+
+
+
+
+
+			board_position.each_with_index do |win_line, index|
+
+				# p win_line
+
+
+
 			
-				if 
-					win_combo.count(marker) == 2 &&win_combo.count('') == 1
-					winning_pos = win_combo.index('') 
+
+
+
+				if win_line.count(marker) == 2 && win_line.count('') == 1
+
+					winning_pos = win_line.index('')
+
 					results = win_array[index][winning_pos]
 
-				elsif win_combo.count(opponent) == 2 && win_combo.count('') == 1
-					winning_pos = win_combo.index('')
-					results = win_array[index][winning_pos] 
 				else 
+
 					results
+
 				end
-			end
+
+		end
+
 		results
+
 	end
 
-	def check_for_fork(ttt_board)
+
+
+	def get_block(ttt_board)
+
+		win_array = [
+
+			[0, 1, 2], 
+
+			[3, 4, 5], 
+
+			[6, 7, 8], 
+
+			[0, 3, 6], 
+
+			[1, 4, 7], 
+
+			[2, 5, 8], 
+
+			[0, 4, 8], 
+
+			[2, 4, 6]
+
+			]
+
+		# win_array.each do |win|
+
+			board_position = [
+
+			[ttt_board[0], ttt_board[1], ttt_board[2]], 
+
+			[ttt_board[3], ttt_board[4], ttt_board[5]], 
+
+			[ttt_board[6], ttt_board[7], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[3], ttt_board[6]], 
+
+			[ttt_board[1], ttt_board[4], ttt_board[7]], 
+
+			[ttt_board[2], ttt_board[5], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[4], ttt_board[8]],
+
+			[ttt_board[2], ttt_board[4], ttt_board[6]]
+
+					]
+
+
+
+				marker1 = 'X'
+
+				marker2 = 'O'
+
+
+
+					if marker == marker1
+
+						opponent = marker2
+
+					else 
+
+						opponent = marker1
+
+					end
+
+				
+
+			results = 9
+
+			board_position.each_with_index do |win_line, index|
+
+				if win_line.count(opponent) == 2 && win_line.count('') == 1
+
+					winning_pos = win_line.index('')
+
+						results = win_array[index][winning_pos]
+
+				else 
+
+					results
+
+				end
+
+			end
+
+				results
+
+	end
+
+
+
+	def win_check_fork(ttt_board)
+
+
+
+
 
 		fork_combinations = [
-							[ttt_board[0],ttt_board[1],ttt_board[2]],
-							[ttt_board[3],ttt_board[4],ttt_board[5]],
-							[ttt_board[6], ttt_board[7], ttt_board[8]],
-							[ttt_board[0], ttt_board[3], ttt_board[6]],
-							[ttt_board[1],ttt_board[4], ttt_board[7]],
-							[ttt_board[2],ttt_board[5],ttt_board[8]], 
-							[ttt_board[0], ttt_board[4], ttt_board[8]],
-							[ttt_board[2],ttt_board[4],ttt_board[6]]
-							]
 
-		fork_positions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+			[ttt_board[0], ttt_board[1], ttt_board[2]], 
+
+			[ttt_board[3], ttt_board[4], ttt_board[5]], 
+
+			[ttt_board[6], ttt_board[7], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[3], ttt_board[6]], 
+
+			[ttt_board[1], ttt_board[4], ttt_board[7]], 
+
+			[ttt_board[2], ttt_board[5], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[4], ttt_board[8]],
+
+			[ttt_board[2], ttt_board[4], ttt_board[6]]
+
+					]
+
+		fork_positions = [
+
+			[0, 1, 2], 
+
+			[3, 4, 5], 
+
+			[6, 7, 8], 
+
+			[0, 3, 6], 
+
+			[1, 4, 7], 
+
+			[2, 5, 8], 
+
+			[0, 4, 8], 
+
+			[2, 4, 6]
+
+			]	
+
+			
 
 		fork_line = []
-		fork_spot = [] # new array for when you flatten
-		i = []  #index positions
-
-		fork_combinations.each_with_index do |forking_line, index|
-			if forking_line.count(marker) == 1 && forking_line.count("") == 2
-				fork_line = forking_line
-				i.push(index)
-			end
-		end 
-
-		i.each do |index|
-			fork_spot << fork_positions[index]
-		end
-		p fork_spot
-		fork_spot = fork_spot.flatten.sort #flattens array, takes forks position arrays, puts in fork positions array
-
-		intersections = []
-		fork_spot.each do |spot| 
-			if ttt_board[spot] == ""
-				intersections.push(spot)
-			end
-		end
-
-		if intersections.detect { |match| intersections.count(match) > 1 } == nil
-
-			move = 10
-
-		else
-			move = intersections.detect { |match| intersections.count(match) > 1}
-		end
-		move
-	end
-
-
-	def block_fork(ttt_board)
-
-		marker_1 = 'X'
-		marker_2 = 'O'
-
-				if marker == marker_1
-					opponent = marker_2
-				else
-					opponent = marker_1
-				end
-
-				 
-		fork_combinations = [
-							[ttt_board[0],ttt_board[1],ttt_board[2]],
-							[ttt_board[3],ttt_board[4],ttt_board[5]],
-							[ttt_board[6], ttt_board[7], ttt_board[8]],
-							[ttt_board[0], ttt_board[3], ttt_board[6]],
-							[ttt_board[1],ttt_board[4], ttt_board[7]],
-							[ttt_board[2],ttt_board[5],ttt_board[8]], 
-							[ttt_board[0], ttt_board[4], ttt_board[8]],
-							[ttt_board[2],ttt_board[4],ttt_board[6]]
-							]
-
-		fork_positions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
 
 		fork_spot = []
 
-		i = []  #index
+		i = []
+
+
 
 		fork_combinations.each_with_index do |forking_line, index|
 
-			if forking_line.count(opponent) == 1 && forking_line.count("") == 2
-					i.push(index)
+
+
+			if forking_line.count(marker) == 1 && forking_line.count('') == 2
+
+				fork_line = forking_line
+
+				i.push(index)
+
 			end
 
 		end
 
 		i.each do |index|
+
 			fork_spot << fork_positions[index]
+
 		end
-		fork_spot = fork_spot.flatten.sort #flattens array, takes forks position arrays, puts in fork positions array
+
+
+
+
+
+		fork_spot = fork_spot.flatten.sort
+
+
 
 		intersections = []
-		fork_spot.each do |spot| 
-				if ttt_board[spot] == ""
-						intersections.push(spot)
-				end
+
+		
+
+		fork_spot.each do |spot|
+
+			if ttt_board[spot] == ''
+
+				intersections.push(spot)
+
+			end
+
 		end
 
-		if ttt_board == ['', '', opponent, '', marker, '', opponent, '', '']
-			move = 3
+
+
+		if intersections.detect { |match| intersections.count(match) > 1 } == nil
+
+				move = 9
+
+			
+
+			else
+
+				move = intersections.detect { |match| intersections.count(match) > 1 }
+
 		
+
+		end
+
+	move
+
+
+
+
+
+	end
+
+
+
+	def block_check_fork(ttt_board)
+
+
+
+		opponent = 'X'
+
+
+
+		if marker == 'X'
+
+			opponent = 'O'
+
+
+
+		else 
+
+			opponent = 'X'
+
+		end
+
+
+
+		fork_combinations = [
+
+			[ttt_board[0], ttt_board[1], ttt_board[2]], 
+
+			[ttt_board[3], ttt_board[4], ttt_board[5]], 
+
+			[ttt_board[6], ttt_board[7], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[3], ttt_board[6]], 
+
+			[ttt_board[1], ttt_board[4], ttt_board[7]], 
+
+			[ttt_board[2], ttt_board[5], ttt_board[8]], 
+
+			[ttt_board[0], ttt_board[4], ttt_board[8]],
+
+			[ttt_board[2], ttt_board[4], ttt_board[6]]
+
+					]
+
+		fork_positions = [
+
+			[0, 1, 2], 
+
+			[3, 4, 5], 
+
+			[6, 7, 8], 
+
+			[0, 3, 6], 
+
+			[1, 4, 7], 
+
+			[2, 5, 8], 
+
+			[0, 4, 8], 
+
+			[2, 4, 6]
+
+			]	
+
+			
+
+		# fork_line = []
+
+		fork_spot = []
+
+		i = []
+
+
+
+		fork_combinations.each_with_index do |forking_line, index|
+
+
+
+			if forking_line.count(opponent) == 1 && forking_line.count('') == 2
+
+				# fork_line = forking_line
+
+				i.push(index)
+
+			end
+
+
+
+		end
+
+
+
+		i.each do |index|
+
+			fork_spot << fork_positions[index]
+
+		end
+
+
+
+		fork_spot = fork_spot.flatten.sort
+
+
+
+		intersections = []
+
+		
+
+		fork_spot.each do |spot|
+
+			
+
+			if ttt_board[spot] == ''
+
+				intersections.push(spot)
+
+			end
+
+
+
+		end
+
+
+
+		if ttt_board == ['', '', opponent, '', marker, '', opponent, '', '']
+
+			move = 3
+
+
+
 		elsif ttt_board == [opponent, '', '', '', marker, '', '', '', opponent]
-			move = 3 
+
+
+
+			move = 3
+
+
+
+
 
 		elsif intersections.detect { |match| intersections.count(match) > 1 } == nil
 
-			move = 10
+				move = 9
 
-		else
-			move = intersections.detect { |match| intersections.count(match) > 1}
+			
+
+			else
+
+				move = intersections.detect { |match| intersections.count(match) > 1 }
+
+		
+
 		end
-		move
+
+
+
 	end
+
+
 
 	def take_center(ttt_board)
-		if ttt_board[4] == ""
+
+		if ttt_board[4] == ''
+
 			move = 4
+
 		else
-			move = 10
+
+			move = 9
+
 		end
+
+		
 
 	end
 
-	def opposite_corner(ttt_board)
 
-		marker_1 = 'X'
-		marker_2 = 'O'
 
-				if marker == marker_1
-					opponent = marker_2
+	def take_corner(ttt_board)
 
-				else
-					opponent = marker_1
-				end
 
-		if ttt_board[0] == opponent && ttt_board[8] == ""
+
+			opponent = 'X'
+
+
+
+		if marker == 'X'
+
+			opponent = 'O'
+
+
+
+		else 
+
+			opponent = 'X'
+
+		end
+
+
+
+		if ttt_board[0] == opponent && ttt_board[8] == ''
+
 			move = 8
 
-		elsif ttt_board[2] == opponent && ttt_board[6] == ""
-			move = 6
-		
-		elsif ttt_board[6] == opponent && ttt_board[2] == ""
+		elsif 
+
+			ttt_board[6] == opponent && ttt_board[2] == ''
+
 			move = 2
 
-		elsif ttt_board[8] == opponent && ttt_board[0] == ""
+		elsif 
+
+			ttt_board[8] == opponent && ttt_board[0] == ''
+
 			move = 0
 
-		else
+
+
+		elsif 
+
+			ttt_board[2] == opponent && ttt_board[6] == ''
+
+			move = 2
+
+		else 
+
 			move = 10
+
 		end
+
+		
 
 	end
 
 
 
-	#def empty_corner(ttt_board)
+	def take_empty_corner(ttt_board)
+
+		empty_corner = []
+
+		corners = [0, 2, 6, 8]
 
 
 
-		#corners = [0,2,6,8]
+		corners.each do |empty|
 
-		#marker = []
+			if ttt_board[empty] == ''
+
+			empty_corner << empty
+
+			end
+
+
+
+		end
+
+
+
+		if empty_corner.length > 0
+
+			move = empty_corner.shift
+
+		else
+
+			move = 10
+
+		end
 
 		
 
-	#end
+	end
+
+
+
+	def take_empty_side(ttt_board)
+
+		empty_side = []
+
+		sides = [1, 3, 5, 7]
+
+
+
+		sides.each do |em|
+
+			if ttt_board[em] == ''
+
+			empty_side << em
+
+			end
+
+		end
+
+
+
+		if empty_side.length > 0
+
+			move = empty_side.shift
+
+		else
+
+			move = 10
+
+		end
+
+	end
+
+
 
 
 
 end
+
